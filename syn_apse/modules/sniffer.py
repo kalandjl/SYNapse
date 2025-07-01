0e0b0fc78434b99ab3fcb17b7309e065b954325d
Kalan Jarvis-Loewen <112147975+kalandjl@users.noreply.github.com>
2025-07-01 15:02:11 -0700

    Made sniffer.py a proper module

--- syn_apse/modules/sniffer.py
+++ syn_apse/modules/sniffer.py
@@ -1,6 +1,27 @@
-from scapy.all import * 
+# syn_apse/modules/sniffer.py
+import scapy.all as scapy
 
-def packet_callback(packet):
+def _packet_callback(packet):
+    """
+    Callback function for every new sniffed packet.
+    .show() provides a detailed, multi-line breakdown.
+    """
     packet.show()
 
-results = sniff(filter="icmp", prn=packet_callback)
+def start_sniffing(interface, filter_str=None, count=0):
+    """
+    Main function for sniffer module
+    """
+
+    print(f"[*] Starting sniffer on interface '{interface}'...")
+    
+    try:
+        scapy.sniff(
+            iface=interface,
+            filter=filter_str,
+            prn=_packet_callback,
+            count=count,
+            store=False
+        )
+    except Exception as e:
+        print(f"[ERROR] Sniffer failed to start: {e}")
